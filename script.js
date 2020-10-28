73e9a05c083a5443dcddabc553362d4ac0c7cee4
sstephensMCSE <sstephens.mcse@gmail.com>
2020-10-28 11:01:40 -0400

    finished the 5 day forecast

--- script.js
+++ script.js
@@ -45,9 +45,7 @@ $("#clearHistory").on("click", function (event) {
 
 $("#searchHistory").on('click', '.btn', function(event) {
     event.preventDefault();
-    console.log($(this).text());
     updateDashboard($(this).text());
-    
 });
 
 function updateDashboard(cityInput) {
@@ -73,7 +71,7 @@ function updateDashboard(cityInput) {
         var temp = $("<p>").text("Temprature: " + response.main.temp);
         var humidity = $("<p>").text("Humidity: " + response.main.humidity);
         var wind = $("<p>").text("Wind Speed: " + response.wind.speed);
-        var weathericon = response.weather[0].main;        
+        var weatherStatus = $("<p>").text("Currently: " + response.weather[0].main);    
         var newDiv = $('<div>');
         newDiv.append(displayDate, temp, humidity, wind);
         $("#currentWeather").html(newDiv);        
@@ -100,8 +98,16 @@ function updateDashboard(cityInput) {
             var newDiv = $('<div>');
             newDiv.append(uvindex);
             $("#uvindex").html(newDiv);
-        });        
-        newDiv.append(displayDate, temp, humidity, wind, uvindex);
+        });       
+        
+
+        // Weather ICON
+        var weathericon= response.weather[0].icon;
+
+        icon = $('<img>').attr("src", "https://openweathermap.org/img/wn/"+weathericon +"@2x.png");            
+        icon.attr("style", "height: 70px; width: 70px");
+
+        newDiv.append(displayDate, icon, weatherStatus, temp, humidity, wind, uvindex);
         $("#currentWeather").html(newDiv);
     });
     
@@ -119,9 +125,32 @@ function updateDashboard(cityInput) {
 
         console.log(response);
         console.log(forecastURL);
-        
-        forecastdiv = $('<div class="col-sm-2 bg-primary forecast text-white ml-2 mb-3 p-2 mt-2 rounded">');      
-        
+
+        for (var i = 0; i < results.length; i += 8) {
+            // Creating a div with style and padding         
+            var fiveDayDiv = $('<div class="col-sm-2 forecast text-white ml-3 mt-3 rounded" style="background-color:#138496;">');
+
+            // Get the date and format            
+            var date = new Date((results[i].dt)*1000).toLocaleDateString();
+
+            // Get temp and humidity
+            var temp = results[i].main.temp;
+            var hum = results[i].main.humidity;
+
+            // Create the icon
+            var weathericon= results[i].weather[0].icon;
+            icon = $('<img>').attr("src", "https://openweathermap.org/img/wn/"+weathericon +"@2x.png");            
+            icon.attr("style", "height: 40px; width: 40px");
+
+            // Creating tags with the result items information.....
+            var h5date = $("<p class='card-title'>").text(date);
+            var pTemp = $("<p class='card-text'>").text("Temp: " + temp);;
+            var pHum = $("<p class='card-text'>").text("Humidity " + hum);;
+
+            // Add to the forecast div
+            fiveDayDiv.append(h5date, icon, pTemp, pHum);            
+            $("#forecast").append(fiveDayDiv);
+        }
 
     });
 };
